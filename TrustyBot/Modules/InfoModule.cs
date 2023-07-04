960b0c2152f0bbb9652b1a3567355bb2354782a5
Ryan Flores <rryanflorres@gmail.com>
2023-07-03 19:21:14 -0700

    Initial Commit

---
+++ TrustyBot/Modules/InfoModule.cs
@@ -0,0 +1,9 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+
+public class InfoModule : ModuleBase<SocketCommandContext>
+{
+    [Command("say")]
+    [Summary("Echoes a message.")]
+    public Task SayAsync([Remainder][Summary("The text to echo")] string echo) => ReplyAsync(echo);
+}
---
+++ TrustyBot/Program.cs
@@ -0,0 +1,84 @@
+﻿using Discord.WebSocket;
+using Discord;
+using Discord.Net;
+using Newtonsoft.Json;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient client;
+    string token = Environment.GetEnvironmentVariable("BOT_TOKEN");
+    const ulong GUILD_ID = 1125167218125181071;
+
+    public async Task MainAsync()
+    {
+        client = new DiscordSocketClient();
+
+        client.Log += Log;
+        client.Ready += Client_Ready;
+        client.SlashCommandExecuted += SlashCommandHandler;
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+        await Task.Delay(-1);
+    }
+    
+
+
+    public async Task Client_Ready()
+    {
+        var guild = client.GetGuild(GUILD_ID);
+
+        var guildCommand = new SlashCommandBuilder()
+            .WithName("list-roles")
+            .WithDescription("This is my first guild slash command!")
+            .AddOption("user", ApplicationCommandOptionType.User, "The users whos roles you want to be listed", isRequired: true);
+
+        try
+        {
+            // Now that we have our builder, we can call the CreateApplicationCommandAsync method to make our slash command.
+            await guild.CreateApplicationCommandAsync(guildCommand.Build());
+        }
+        catch (HttpException exception)
+        {
+            var json = JsonConvert.SerializeObject(exception.Reason, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+
+    private async Task SlashCommandHandler(SocketSlashCommand command)
+    {
+        switch (command.Data.Name)
+        {
+            case "list-roles":
+                await HandleListRoleCommand(command);
+                break;
+        }
+    }
+
+    private async Task HandleListRoleCommand(SocketSlashCommand command)
+    {
+        // We need to extract the user parameter from the command. since we only have one option and it's required, we can just use the first option.
+        var guildUser = (SocketGuildUser)command.Data.Options.First().Value;
+
+        // We remove the everyone role and select the mention of each role.
+        var roleList = string.Join(",\n", guildUser.Roles.Where(x => !x.IsEveryone).Select(x => x.Mention));
+
+        var embedBuiler = new EmbedBuilder()
+            .WithAuthor(guildUser.ToString(), guildUser.GetAvatarUrl() ?? guildUser.GetDefaultAvatarUrl())
+            .WithTitle("Roles")
+            .WithDescription(roleList)
+            .WithColor(Color.Green)
+            .WithCurrentTimestamp();
+
+        // Now, Let's respond with the embed.
+        await command.RespondAsync(embed: embedBuiler.Build());
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
